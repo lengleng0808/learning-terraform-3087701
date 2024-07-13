e3221c1da83e41b4f5f6d86cf9d0f6c6f21afde2
lengleng0808 <121216919+lengleng0808@users.noreply.github.com>
2024-07-13 19:35:45 +0800

    Create security group

--- main.tf
+++ main.tf
@@ -19,9 +19,8 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
@@ -53,7 +52,7 @@ resource "aws_security_group_rule" blog_https_in" {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group_blog.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" blog_everything_out" {
@@ -63,5 +62,5 @@ resource "aws_security_group_rule" blog_everything_out" {
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group_blog.id
+  security_group_id = aws_security_group.blog.id
 }
