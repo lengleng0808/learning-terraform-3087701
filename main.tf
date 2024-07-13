b83fedc32156e05361db91d61e595545d0f22b7a
lengleng0808 <121216919+lengleng0808@users.noreply.github.com>
2024-07-13 22:07:13 +0800

    Use module for sg2

--- main.tf
+++ main.tf
@@ -32,15 +32,15 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
-  name = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+
+  vpc_id  = data.aws_vpc.default.id
+  name    = "blog_new"
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  ingress_rules       = ["all-all"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]  
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]  
 }
 
 
